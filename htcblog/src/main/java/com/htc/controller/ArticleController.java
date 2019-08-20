32f3b737b7feb3d3fe89e86b48c8759abec44ceb
huangtiancai <1812869775@qq.com>
2019-08-20 22:37:34 +0800

    commit

--- htcblog/src/main/java/com/htc/controller/ArticleController.java
+++ htcblog/src/main/java/com/htc/controller/ArticleController.java
@@ -3,16 +3,6 @@
  */
 package com.htc.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.htc.service.ArticleService;
-import com.htc.service.CategoryService;
-import com.htc.service.CommentService;
-import com.htc.service.TagService;
-import com.htc.service.UserService;
-
 /**  
 * @ClassName ArticleController
 * @Description:文章的控制层
@@ -20,7 +10,6 @@ import com.htc.service.UserService;
 * @date 2019年8月20日下午1:35:37 
 */
 
-@Controller
 public class ArticleController {
 //	@Autowired
 //	private ArticleService articleService;
